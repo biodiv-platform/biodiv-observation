0b983bd749040d24954e0b76ed91ee6e6767532d
Harsh Zalavadiya <harsh@strandls.com>
2019-10-04 15:52:09 +0530

    :green_heart: updated CD to use latest maven-toolbox

---
+++ @ci/build-and-deploy.sh
@@ -0,0 +1,11 @@
+# Download maven-toolbox
+curl -L http://bit.ly/maven-toolbox | bash
+chmod +x @ci/maven-toolbox
+
+# Configure and build project
+./@ci/maven-toolbox configure-properties src/main/resources/config.properties
+./@ci/maven-toolbox configure-m2
+./@ci/maven-toolbox configure-hibernate
+mvn clean install
+./@ci/maven-toolbox configure-sdk
+mvn -f target/sdk/pom.xml deploy
