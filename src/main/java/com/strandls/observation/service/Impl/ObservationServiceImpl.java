28da4e797ba3deb197834fd43f29128599b7540c
akrudra <abhishek.rudra02@gmail.com>
2020-03-26 13:05:39 +0530

    userScore null check

--- src/main/java/com/strandls/observation/service/Impl/ObservationServiceImpl.java
+++ src/main/java/com/strandls/observation/service/Impl/ObservationServiceImpl.java
@@ -189,7 +189,9 @@ public class ObservationServiceImpl implements ObservationService {
 			try {
 				in.close();
 				UserScore score = esService.getUserScore("eaf", "er", observation.getAuthorId().toString());
-				authorScore = score.getRecord().get(0).get("details");
+				if(!score.getRecord().isEmpty()) {
+					authorScore = score.getRecord().get(0).get("details");
+				}
 				facts = traitService.getFacts("species.participation.Observation", id.toString());
 				observationResource = resourceService.getImageResource(id.toString());
 				userGroups = userGroupService.getObservationUserGroup(id.toString());
