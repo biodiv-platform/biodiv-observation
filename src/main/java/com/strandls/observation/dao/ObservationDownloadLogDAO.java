b9612a84f09d8f2cd2a51edae610f7b6249407ed
Abhishek Kumar Rudra <abhishek.rudra02@gmail.com>
2020-06-02 11:40:16 +0530

    inject mothod resolved

--- src/main/java/com/strandls/observation/dao/ObservationDownloadLogDAO.java
+++ src/main/java/com/strandls/observation/dao/ObservationDownloadLogDAO.java
@@ -7,7 +7,8 @@ import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.inject.Inject;
+import javax.inject.Inject;
+
 import com.strandls.observation.pojo.DownloadLog;
 import com.strandls.observation.util.AbstractDAO;
 
