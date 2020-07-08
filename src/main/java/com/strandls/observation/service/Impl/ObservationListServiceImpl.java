0972314d18dbfcfc1d4496009881b95f70a89873
Abhishek Kumar Rudra <abhishek.rudra02@gmail.com>
2020-07-08 23:51:35 +0530

    capitalize trait

--- src/main/java/com/strandls/observation/service/Impl/ObservationListServiceImpl.java
+++ src/main/java/com/strandls/observation/service/Impl/ObservationListServiceImpl.java
@@ -13,6 +13,7 @@ import java.util.Map.Entry;
 
 import javax.inject.Inject;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -453,7 +454,8 @@ public class ObservationListServiceImpl implements ObservationListService {
 
 		for (Entry<String, Long> entry : aggregation.entrySet()) {
 			if (entry.getKey().split("\\|")[0].equalsIgnoreCase(traitName)) {
-				traitsAgg.put(entry.getKey().split("\\|")[1], entry.getValue());
+				String capitalizeWord = StringUtils.capitalize(entry.getKey().split("\\|")[1]);
+				traitsAgg.put(capitalizeWord, entry.getValue());
 			}
 		}
 		return traitsAgg;
