beef41cb12483f7b5dc166ed4903ec919a65c984
Sethuraman Srinivasan <sethu10121994@gmail.com>
2020-08-05 19:14:29 +0530

    :recycle: admin mail fix

--- src/main/java/com/strandls/observation/service/Impl/MailServiceImpl.java
+++ src/main/java/com/strandls/observation/service/Impl/MailServiceImpl.java
@@ -22,45 +22,39 @@ import com.strandls.user.controller.UserServiceApi;
 import com.strandls.user.pojo.User;
 
 public class MailServiceImpl implements MailService {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(MailServiceImpl.class);
-	
+
 	@Inject
 	private Channel channel;
-	
+
 	@Inject
 	private UserServiceApi userServiceApi;
-	
+
 	@Override
 	public void sendMail(String authorId, String fileName, String type) {
 		try {
 			User user = userServiceApi.getUser(authorId);
-		
-		Properties properties = PropertyFileUtil.fetchProperty("config.properties");
-		Map<String, Object> data = new HashMap<String, Object>();
-		data.put(FIELDS.TYPE.getAction(), MAIL_TYPE.DOWNLOAD_MAIL.getAction());
-		data.put(FIELDS.TO.getAction(), new String[] {user.getEmail()});
-		Map<String, Object> model = new HashMap<String, Object>();
-		model.put(DOWNLOAD_MAIL.SERVER_URL.getAction(), properties.getProperty("serverUrl"));
-		model.put(DOWNLOAD_MAIL.SITENAME.getAction(), properties.getProperty("siteName"));
-		model.put(DOWNLOAD_MAIL.USER_DATA.getAction(), user);
-		model.put(DOWNLOAD_MAIL.DOWNLOAD_TYPE.getAction(), type);
-		model.put(DOWNLOAD_MAIL.DOWNLOAD_FILE.getAction(), fileName);
-		model.put(DOWNLOAD_MAIL.TYPE.getAction(), MAIL_TYPE.DOWNLOAD_MAIL.getAction());
-		data.put(FIELDS.DATA.getAction(), JsonUtil.unflattenJSON(model));
-		RabbitMQProducer producer = new RabbitMQProducer(channel);
-		if (user.getEmail() != null && !user.getEmail().isEmpty()
-				&& !user.getEmail().contains("@ibp.org") && user.getSendNotification() != null
-				&& user.getSendNotification()) {
-			producer.produceMail(RabbitMqConnection.EXCHANGE_BIODIV, RabbitMqConnection.MAIL_ROUTING_KEY, null,
-					JsonUtil.mapToJSON(data));
-		}
-			String admins = PropertyFileUtil.fetchProperty("config.properties", "mail_bcc");
-			data.put(FIELDS.TO.getAction(), admins.split(","));
-			producer.produceMail(RabbitMqConnection.EXCHANGE_BIODIV, RabbitMqConnection.MAIL_ROUTING_KEY, null,
-					JsonUtil.mapToJSON(data));
-	}
-		 catch (Exception e) {
+
+			Properties properties = PropertyFileUtil.fetchProperty("config.properties");
+			Map<String, Object> data = new HashMap<String, Object>();
+			data.put(FIELDS.TYPE.getAction(), MAIL_TYPE.DOWNLOAD_MAIL.getAction());
+			data.put(FIELDS.TO.getAction(), new String[] { user.getEmail() });
+			data.put(FIELDS.SUBSCRIPTION.getAction(), user.getSendNotification());
+			Map<String, Object> model = new HashMap<String, Object>();
+			model.put(DOWNLOAD_MAIL.SERVER_URL.getAction(), properties.getProperty("serverUrl"));
+			model.put(DOWNLOAD_MAIL.SITENAME.getAction(), properties.getProperty("siteName"));
+			model.put(DOWNLOAD_MAIL.USER_DATA.getAction(), user);
+			model.put(DOWNLOAD_MAIL.DOWNLOAD_TYPE.getAction(), type);
+			model.put(DOWNLOAD_MAIL.DOWNLOAD_FILE.getAction(), fileName);
+			model.put(DOWNLOAD_MAIL.TYPE.getAction(), MAIL_TYPE.DOWNLOAD_MAIL.getAction());
+			data.put(FIELDS.DATA.getAction(), JsonUtil.unflattenJSON(model));
+			RabbitMQProducer producer = new RabbitMQProducer(channel);
+			if (user.getEmail() != null && !user.getEmail().isEmpty() && !user.getEmail().contains("@ibp.org")) {
+				producer.produceMail(RabbitMqConnection.EXCHANGE_BIODIV, RabbitMqConnection.MAIL_ROUTING_KEY, null,
+						JsonUtil.mapToJSON(data));
+			}
+		} catch (Exception e) {
 			logger.error(e.getMessage());
 		}
 	}
