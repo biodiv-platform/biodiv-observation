2277b652d846e955d39a5d3dc99d9a7842c39070
prakhar-s <prakhardevsingh4@gmail.com>
2023-09-21 13:48:55 +0000

    refactor: :recycle: put layer info pull function as env variable

--- src/main/java/com/strandls/observation/es/util/ConstructESDocument.java
+++ src/main/java/com/strandls/observation/es/util/ConstructESDocument.java
@@ -29,6 +29,8 @@ public class ConstructESDocument {
 	private String classificationId = PropertyFileUtil.fetchProperty("config.properties", "classificationId");
 
 	private String locationInfoLayer = PropertyFileUtil.fetchProperty("config.properties", "locationinfo_layer_view");
+	
+	private String pull_function=PropertyFileUtil.fetchProperty("config.properties", "pull_function");
 
 	public List<ObservationESDocument> getESDocumentStub(String observationId) {
 
@@ -75,7 +77,7 @@ public class ConstructESDocument {
 				+ "(SELECT id s_id, name created_by, COALESCE(profile_pic,  icon) profile_pic FROM suser ) U ON U.s_id = O.author_id "
 				+ "LEFT OUTER JOIN "
 				+ "(SELECT id s_id, name group_name, group_order from species_group )S ON S.s_id = O.group_id "
-				+ "LEFT OUTER JOIN " + "(SELECT lon, lat, public.ibp_tahsil_pull(lon, lat) as location_information FROM "
+				+ "LEFT OUTER JOIN " + "(SELECT lon, lat, public."+pull_function+"(lon, lat) as location_information FROM "
 				+ "(SELECT longitude,latitude FROM observation where id in("+observationId+")) AS input(lon, lat)) L ON L.lon = O.longitude  " + "AND L.lat = O.latitude " + " "
 				+ "LEFT OUTER JOIN " + "( " + "SELECT  " + "observation_id,  "
 				+ "(reco_vote->>'recommendation_id')\\:\\:bigint AS recommendation_id, " + "CASE "
