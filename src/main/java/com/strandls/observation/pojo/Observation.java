128307b080550ac1b04dd2c2e67c2f3741bbd100
prakhar-s <prakhardevsingh4@gmail.com>
2025-10-04 15:27:21 +0530

    refactor: :recycle: addeed missing import

--- src/main/java/com/strandls/observation/pojo/Observation.java
+++ src/main/java/com/strandls/observation/pojo/Observation.java
@@ -15,6 +15,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
 /**
