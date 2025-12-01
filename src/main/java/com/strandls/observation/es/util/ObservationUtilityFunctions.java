47d074a79b09b686466afbfd34df5fee59eac823
prakhar-s <prakhardevsingh4@gmail.com>
2025-12-01 18:04:53 +0530

    disabling bgposting to debug datatable create issue

--- src/main/java/com/strandls/observation/es/util/ObservationUtilityFunctions.java
+++ src/main/java/com/strandls/observation/es/util/ObservationUtilityFunctions.java
@@ -360,7 +360,8 @@ public class ObservationUtilityFunctions {
 				mapper.createUserGroupMapping(requestAuthHeader, fieldMapping, dataRow,
 						observationData.getUserGroupsList(), userGroup, observation.getId());
 				mapper.updateGeoPrivacy(observation);
-				mapper.updateUserGroupFilterForDatatable(requestAuthHeader, observation);
+				// COMMENTED OUT FOR TESTING - disables background posting to user groups via integrator
+				// mapper.updateUserGroupFilterForDatatable(requestAuthHeader, observation);
 				return observation.getId();
 			}
 
